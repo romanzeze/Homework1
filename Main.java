41068ea3bc1677cefc9fef6756e86942df9885e0
Roman <rzozulya1997@gmail.com>
2023-02-16 16:36:01 +0200

    complete tasks

--- Main.java
+++ Main.java
@@ -10,7 +10,8 @@ public class Main {
         secondTaskFromHomeWork();
         thirdTaskFromHomeWork();
     }
-    public static void fistTaskFromPracticalTask(){
+
+    public static void fistTaskFromPracticalTask() {
         System.out.println("input text");
         Scanner sc = new Scanner(System.in);
         String firstText = sc.nextLine();
@@ -21,7 +22,8 @@ public class Main {
         System.out.println(secondText);
         System.out.println(firstText);
     }
-    public static void secondTaskFromPracticalTask(){
+
+    public static void secondTaskFromPracticalTask() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("input first number");
         int x = scanner.nextInt();
@@ -29,51 +31,56 @@ public class Main {
         int y = scanner.nextInt();
         System.out.println("input third number");
         int z = scanner.nextInt();
-        double avarage= ((x +y +z)/3);
-        System.out.println("avarage:" +avarage );
+        double avarage = ((x + y + z) / 3);
+        System.out.println("avarage:" + avarage);
     }
-    public static void thirdTaskFromPracticalTask(){
+
+    public static void thirdTaskFromPracticalTask() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("input a");
         int a = scanner.nextInt();
         System.out.println("input b");
         int b = scanner.nextInt();
-        int addition = a+b;
-        int subtraction= a-b;
-        int multiplication = a*b;
-        int division = a/b;
-        System.out.println("addition a+b:"+ addition );
-        System.out.println("subtraction a-b:"+ subtraction);
-        System.out.println("multiplication a*b:"+ multiplication);
-        System.out.println("division a/b:"+ division);
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+        System.out.println("addition a+b:" + addition);
+        System.out.println("subtraction a-b:" + subtraction);
+        System.out.println("multiplication a*b:" + multiplication);
+        System.out.println("division a/b:" + division);
     }
-    public static void fourthTaskFromPracticalTask(){
+
+    public static void fourthTaskFromPracticalTask() {
         System.out.println("How are you?");
-        String answer= "good";
-        System.out.println("You are "+answer);
+        String answer = "good";
+        System.out.println("You are " + answer);
 
     }
-    public static void firstTaskFromHomeWork(){
+
+    public static void firstTaskFromHomeWork() {
         System.out.print("Enter radius:");
         Scanner scanner = new Scanner(System.in);
         int r = scanner.nextInt();
-        double s = 3.14*Math.pow(r,2) ;
-        double p = 2*3.14*r;
-        System.out.println("perimeter:"+p);
-        System.out.println("area:"+s);
+        double s = 3.14 * Math.pow(r, 2);
+        double p = 2 * 3.14 * r;
+        System.out.println("perimeter:" + p);
+        System.out.println("area:" + s);
 
     }
-    public static void secondTaskFromHomeWork(){
-        String name= "Roman";
-        String adress ="Lviv";
+
+    public static void secondTaskFromHomeWork() {
+        String name = "Roman";
+        String adress = "Lviv";
         System.out.println("What is your name? ");
-        System.out.println("My name is "+name);
-        System.out.println("Where are you live "+ name +"?");
-        System.out.println("Your name is "+name);
+        System.out.println("My name is " + name);
+        System.out.println("Where are you live " + name + "?");
+        System.out.println("Your name is " + name);
         System.out.println("Your adress is " + adress);
 
     }
-    public static void thirdTaskFromHomeWork(){
+
+    public static void thirdTaskFromHomeWork() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter how long the first call lasted:");
         int t1 = sc.nextInt();
@@ -87,13 +94,13 @@ public class Main {
         int t3 = sc.nextInt();
         System.out.print("Enter third tariff:");
         int c3 = sc.nextInt();
-        int r1=t1+c1;
-        int r2=t2+c2;
-        int r3=t3+c3;
-        int allR= r1+r2+r3;
-        System.out.println("The first call cost "+r1);
-        System.out.println("The second call cost "+r2);
-        System.out.println("The third call cost "+r3);
+        int r1 = t1 + c1;
+        int r2 = t2 + c2;
+        int r3 = t3 + c3;
+        int allR = r1 + r2 + r3;
+        System.out.println("The first call cost " + r1);
+        System.out.println("The second call cost " + r2);
+        System.out.println("The third call cost " + r3);
         System.out.println("the total cost is " + allR);
 
     }
